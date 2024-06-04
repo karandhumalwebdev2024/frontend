7eab70f3ad6651068e7b658061cd13ff43c0fee8
karan dhumal <karandhumalwebdev2024@gmail.com>
2024-06-04 15:16:32 +0530

    Add files via upload

---
+++ HTML CSS + JS/bubble game using js/script.js
@@ -0,0 +1,53 @@
+var timer =60;
+var score =0;
+var hitrn =0;
+
+function incresescore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score; 
+}
+
+function getnewhit(){
+    hitrn =  Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+function makebubble(){
+    var clutter = "";
+
+    for(var i=1; i<=133; i++){
+        var rn = Math.floor(Math.random()*10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+    }
+function runtimer(){
+    var timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else{
+            clearInterval(timerint)
+            document.querySelector("#pbtm").innerHTML="";    
+        }
+    },1000)
+}
+
+document.querySelector("#pbtm")
+.addEventListener("click",function(dets){
+var clickednum = Number(dets.target.textContent);
+if(clickednum === hitrn){
+    incresescore();
+    makebubble();
+    getnewhit();
+}
+else{
+    
+}
+});
+
+
+getnewhit()
+makebubble()
+runtimer()
+
